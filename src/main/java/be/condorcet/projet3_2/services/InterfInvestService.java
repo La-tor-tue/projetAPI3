b86f2e21e01e9b9537f24c5dab3a12f8754b1076
Brandon <brandon.moriau@condorcet.be>
2022-11-01 15:40:38 +0100

    Init Interfaces pour Travail.java et Invest.java

---
+++ src/main/java/be/condorcet/projet3_2/services/InterfInvestService.java
@@ -0,0 +1,6 @@
+package be.condorcet.projet3_2.services;
+
+import be.condorcet.projet3_2.entities.Invest;
+
+public interface InterfInvestService extends InterfService<Invest>{
+}
---
+++ src/main/java/be/condorcet/projet3_2/services/InterfTravailService.java
@@ -0,0 +1,6 @@
+package be.condorcet.projet3_2.services;
+
+import be.condorcet.projet3_2.entities.Travail;
+
+public interface InterfTravailService extends InterfService<Travail>{
+}
