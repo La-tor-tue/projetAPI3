0f00add34ea67add9bc83ac34c53f8b3886b95fd
Brandon <brandon.moriau@condorcet.be>
2022-11-12 18:05:06 +0100

    Methode read par nom

--- src/main/java/be/condorcet/projet3_2/repositories/EmployeRepository.java
+++ src/main/java/be/condorcet/projet3_2/repositories/EmployeRepository.java
@@ -1,9 +1,15 @@
 package be.condorcet.projet3_2.repositories;
 
+import be.condorcet.projet3_2.entities.Discipline;
 import be.condorcet.projet3_2.entities.Employe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeRepository extends JpaRepository<Employe, Integer> {
+    List<Employe> findEmployesByEmpNomLike(String nom);
+
+    List<Employe> findEmployesByEmpDisLike(Discipline discipline);
 }
--- src/main/java/be/condorcet/projet3_2/repositories/ProjetRepository.java
+++ src/main/java/be/condorcet/projet3_2/repositories/ProjetRepository.java
@@ -3,5 +3,9 @@ package be.condorcet.projet3_2.repositories;
 import be.condorcet.projet3_2.entities.Projet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjetRepository extends JpaRepository<Projet,Integer> {
+
+    List<Projet> findProjetsByPjTitreLike(String titre);
 }
